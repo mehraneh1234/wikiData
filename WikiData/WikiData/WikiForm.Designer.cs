1eed430d1f111ae2a604c3cf6d796be62528dbd4
mehraneh1234 <132540431+mehraneh1234@users.noreply.github.com>
2023-08-08 22:51:00 +0800

    Add and Edit Buttons

---
+++ WikiData/WikiData/WikiForm.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace WikiData
+{
+    partial class WikiForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.buttonLoad = new System.Windows.Forms.Button();
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.buttonEdit = new System.Windows.Forms.Button();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.txtDefinition = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtStructure = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtCategory = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtDataStrName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lstDataStructure = new System.Windows.Forms.ListBox();
+            this.StatusStripDataStr = new System.Windows.Forms.StatusStrip();
+            this.SuspendLayout();
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(915, 66);
+            this.buttonSave.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(156, 35);
+            this.buttonSave.TabIndex = 34;
+            this.buttonSave.Text = "Save";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.ButtonSave_Click);
+            // 
+            // buttonLoad
+            // 
+            this.buttonLoad.Location = new System.Drawing.Point(753, 66);
+            this.buttonLoad.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.buttonLoad.Name = "buttonLoad";
+            this.buttonLoad.Size = new System.Drawing.Size(156, 35);
+            this.buttonLoad.TabIndex = 33;
+            this.buttonLoad.Text = "Load";
+            this.buttonLoad.UseVisualStyleBackColor = true;
+            this.buttonLoad.Click += new System.EventHandler(this.ButtonLoad_Click);
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.Location = new System.Drawing.Point(593, 66);
+            this.buttonSearch.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(152, 35);
+            this.buttonSearch.TabIndex = 32;
+            this.buttonSearch.Text = "Search";
+            this.buttonSearch.UseVisualStyleBackColor = true;
+            this.buttonSearch.Click += new System.EventHandler(this.ButtonSearch_Click);
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(147, 66);
+            this.txtSearch.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(382, 26);
+            this.txtSearch.TabIndex = 31;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(67, 66);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(68, 20);
+            this.label5.TabIndex = 30;
+            this.label5.Text = "Search: ";
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(397, 433);
+            this.buttonDelete.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(154, 35);
+            this.buttonDelete.TabIndex = 29;
+            this.buttonDelete.Text = "Delete";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.ButtonDelete_Click);
+            // 
+            // buttonEdit
+            // 
+            this.buttonEdit.Location = new System.Drawing.Point(234, 433);
+            this.buttonEdit.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.buttonEdit.Name = "buttonEdit";
+            this.buttonEdit.Size = new System.Drawing.Size(156, 35);
+            this.buttonEdit.TabIndex = 28;
+            this.buttonEdit.Text = "Edit";
+            this.buttonEdit.UseVisualStyleBackColor = true;
+            this.buttonEdit.Click += new System.EventHandler(this.ButtonEdit_Click);
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(72, 433);
+            this.buttonAdd.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(156, 35);
+            this.buttonAdd.TabIndex = 27;
+            this.buttonAdd.Text = "Add";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.ButtonAdd_Click);
+            // 
+            // txtDefinition
+            // 
+            this.txtDefinition.Location = new System.Drawing.Point(251, 329);
+            this.txtDefinition.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtDefinition.Name = "txtDefinition";
+            this.txtDefinition.Size = new System.Drawing.Size(278, 26);
+            this.txtDefinition.TabIndex = 26;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(67, 329);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(80, 20);
+            this.label4.TabIndex = 25;
+            this.label4.Text = "Definition:";
+            // 
+            // txtStructure
+            // 
+            this.txtStructure.Location = new System.Drawing.Point(251, 264);
+            this.txtStructure.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtStructure.Name = "txtStructure";
+            this.txtStructure.Size = new System.Drawing.Size(278, 26);
+            this.txtStructure.TabIndex = 24;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(67, 264);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 20);
+            this.label3.TabIndex = 23;
+            this.label3.Text = "Structure:";
+            // 
+            // txtCategory
+            // 
+            this.txtCategory.Location = new System.Drawing.Point(251, 201);
+            this.txtCategory.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtCategory.Name = "txtCategory";
+            this.txtCategory.Size = new System.Drawing.Size(278, 26);
+            this.txtCategory.TabIndex = 22;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(67, 201);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 20);
+            this.label2.TabIndex = 21;
+            this.label2.Text = "Category: ";
+            // 
+            // txtDataStrName
+            // 
+            this.txtDataStrName.Location = new System.Drawing.Point(251, 143);
+            this.txtDataStrName.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtDataStrName.Name = "txtDataStrName";
+            this.txtDataStrName.Size = new System.Drawing.Size(278, 26);
+            this.txtDataStrName.TabIndex = 20;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(67, 143);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(164, 20);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "Data Structure Name:";
+            // 
+            // lstDataStructure
+            // 
+            this.lstDataStructure.FormattingEnabled = true;
+            this.lstDataStructure.ItemHeight = 20;
+            this.lstDataStructure.Location = new System.Drawing.Point(593, 143);
+            this.lstDataStructure.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.lstDataStructure.Name = "lstDataStructure";
+            this.lstDataStructure.Size = new System.Drawing.Size(478, 324);
+            this.lstDataStructure.TabIndex = 18;
+            // 
+            // StatusStripDataStr
+            // 
+            this.StatusStripDataStr.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.StatusStripDataStr.Location = new System.Drawing.Point(0, 509);
+            this.StatusStripDataStr.Name = "StatusStripDataStr";
+            this.StatusStripDataStr.Size = new System.Drawing.Size(1155, 22);
+            this.StatusStripDataStr.TabIndex = 35;
+            this.StatusStripDataStr.Text = "statusStrip1";
+            // 
+            // WikiForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1155, 531);
+            this.Controls.Add(this.StatusStripDataStr);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.buttonLoad);
+            this.Controls.Add(this.buttonSearch);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonEdit);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.txtDefinition);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtStructure);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtCategory);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtDataStrName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lstDataStructure);
+            this.Name = "WikiForm";
+            this.Text = "List of Data Structure";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Button buttonLoad;
+        private System.Windows.Forms.Button buttonSearch;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Button buttonEdit;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.TextBox txtDefinition;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtStructure;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtCategory;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtDataStrName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox lstDataStructure;
+        private System.Windows.Forms.StatusStrip StatusStripDataStr;
+    }
+}
+
---
+++ WikiData/WikiData/WikiForm.cs
@@ -0,0 +1,280 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WikiData
+{
+    public partial class WikiForm : Form
+    {
+        public WikiForm()
+        {
+            InitializeComponent();
+        }
+
+        static int max = 12;        // Maximum number of data structures
+        static int fields = 4;      // Data Structure Name, Category, Structure and Definition
+        int ptr = 0;                // Current length of array with data
+        private string[,] dataStructuresArray = new string[max, fields];
+
+        private void ButtonSearch_Click(object sender, EventArgs e)
+        {
+
+        }
+        // Button method to add a new data structure and sort data
+        private void ButtonAdd_Click(object sender, EventArgs e)
+        {
+            AddData();
+            Clear_TextBoxes();
+            //Bubble_Sort();
+            DisplayListDataStr();
+        }
+
+        // This method add a new item to the 2 dimontional array.
+        private void AddData()
+        {
+            StatusStripDataStr.Items.Clear();
+            if (ptr < max)
+            {
+                try
+                {
+                    dataStructuresArray[ptr, 0] = txtDataStrName.Text;
+                    dataStructuresArray[ptr, 1] = txtCategory.Text;
+                    dataStructuresArray[ptr, 2] = txtStructure.Text;
+                    dataStructuresArray[ptr, 3] = txtDefinition.Text;
+                    ptr++;
+                }
+                catch
+                {
+                    StatusStripDataStr.Text = "Well that didn't work!";
+                }
+            }
+            else
+            {
+                StatusStripDataStr.Text = "The array is full!";
+            }
+        }
+        // Method to clear the input text boxes.
+        private void Clear_TextBoxes()
+        {
+            txtDataStrName.Clear();
+            txtCategory.Clear();
+            txtDefinition.Clear();
+            txtStructure.Clear();
+        }
+        // Method to sort and display the array in the listbox
+    /*    private static void ‌Bubble_Sort()
+        {
+            for (int x = 1; x < max; x++)
+            {
+                for (int i = 0; i < max - 1; i++)
+                {
+                    if (!(string.IsNullOrEmpty(dataStructuresArray[i + 1, 0])))
+                    {
+                        if (string.Compare(dataStructuresArray[i, 0], dataStructuresArray[i + 1, 0]) == 1)
+                        {
+                            Swap(i);
+                        }
+                    }
+
+                }
+            }
+
+        }
+        // Swap Routine
+        private void Swap(int index)
+        {
+            string temp;
+            for (int i = 0; i < fields; i++)
+            {
+                temp = dataStructuresArray[index, i];
+                dataStructuresArray[index, i] = dataStructuresArray[index + 1, i];
+                dataStructuresArray[index + 1, i] = temp;
+            }
+        }
+    */
+        // Display Data Structure Array
+        private void DisplayListDataStr()
+        {
+           // StatusStripDataStr.Items.Clear();
+            string rec = "";
+            if (ptr > 0)
+            {
+                lstDataStructure.Items.Clear();
+                for (int x = 0; x < ptr; x++)
+                {
+                    rec = dataStructuresArray[x, 0] + "\t" + dataStructuresArray[x, 1] + "\t" + dataStructuresArray[x, 2] + "\t" + dataStructuresArray[x, 3];
+                    lstDataStructure.Items.Add(rec);
+                }
+            }
+            else
+            {
+                StatusStripDataStr.Text = "Nothing to display";
+            }
+                
+        }
+
+        private void ButtonEdit_Click(object sender, EventArgs e)
+        {
+            EditData();
+            Clear_TextBoxes();
+            DisplayListDataStr();
+                       
+        }
+        private void EditData()
+        {
+            StatusStripDataStr.Items.Clear();
+            try
+            {
+                
+                string selectedItem = lstDataStructure.SelectedItem.ToString();
+                string ss;
+                //bool found = false;
+                for (int i = 0; i <= ptr; i++)
+                {
+                   
+                    ss = dataStructuresArray[i, 0] + "\t" + dataStructuresArray[i, 1] + "\t" + dataStructuresArray[i, 2] + "\t" + dataStructuresArray[i, 3];
+                    
+
+                    if (selectedItem == ss && txtDataStrName.Text != "" && txtCategory.Text != "" && txtStructure.Text != "" && txtDefinition.Text != "")
+                    {
+                        dataStructuresArray[i, 0] = txtDataStrName.Text;
+                        dataStructuresArray[i, 1] = txtCategory.Text;
+                        dataStructuresArray[i, 2] = txtStructure.Text;
+                        dataStructuresArray[i, 3] = txtDefinition.Text;
+                        StatusStripDataStr.Text = "Data Structure Name, Category, Structure and Definition are edited!";
+                        break;
+                    }
+                    else if (selectedItem == ss && txtDataStrName.Text != "" && txtCategory.Text != "" && txtStructure.Text != "")
+                    {
+                        dataStructuresArray[i, 0] = txtDataStrName.Text;
+                        dataStructuresArray[i, 1] = txtCategory.Text;
+                        dataStructuresArray[i, 2] = txtStructure.Text;
+                        StatusStripDataStr.Text = "Data Structure Name, Category and Structure are edited!";
+                        break;
+                    }
+                    else if (selectedItem == ss && txtDataStrName.Text != "" && txtCategory.Text != "" && txtDefinition.Text != "")
+                    {
+                        dataStructuresArray[i, 0] = txtDataStrName.Text;
+                        dataStructuresArray[i, 1] = txtCategory.Text;
+                        dataStructuresArray[i, 3] = txtDefinition.Text;
+                        StatusStripDataStr.Text = "Data Structure Name, Category and Definition are edited!";
+                        break;
+                    }
+                    else if (selectedItem == ss && txtDataStrName.Text != "" && txtStructure.Text != "" && txtDefinition.Text != "")
+                    {
+                        dataStructuresArray[i, 0] = txtDataStrName.Text;
+                        dataStructuresArray[i, 3] = txtDefinition.Text;
+                        dataStructuresArray[i, 2] = txtStructure.Text;
+                        StatusStripDataStr.Text = "Data Structure Name, Definition and Structure are edited!";
+                        break;
+                    }
+                    else if (selectedItem == ss && txtDefinition.Text != "" && txtCategory.Text != "" && txtStructure.Text != "")
+                    {
+                        dataStructuresArray[i, 3] = txtDefinition.Text;
+                        dataStructuresArray[i, 1] = txtCategory.Text;
+                        dataStructuresArray[i, 2] = txtStructure.Text;
+                        StatusStripDataStr.Text = "Definition, Category and Structure are edited!";
+                        break;
+                    }
+                    else if (selectedItem == ss && txtDataStrName.Text != "" && txtCategory.Text != "")
+                    {
+                        dataStructuresArray[i, 0] = txtDataStrName.Text;
+                        dataStructuresArray[i, 1] = txtCategory.Text;
+                        StatusStripDataStr.Text = "Data Structure Name and Category are edited!";
+                        break;
+                    }
+                    else if (selectedItem == ss && txtStructure.Text != "" && txtCategory.Text != "")
+                    {
+                        dataStructuresArray[i, 2] = txtStructure.Text;
+                        dataStructuresArray[i, 1] = txtCategory.Text;
+                        StatusStripDataStr.Text = "Structure and Category are edited!";
+                        break;
+                    }
+                    else if (selectedItem == ss && txtDefinition.Text != "" && txtCategory.Text != "")
+                    {
+                        dataStructuresArray[i, 3] = txtDefinition.Text;
+                        dataStructuresArray[i, 1] = txtCategory.Text;
+                        StatusStripDataStr.Text = "Definition and Category are edited!";
+                        break;
+                    }
+                    else if (selectedItem == ss && txtDataStrName.Text != "" && txtStructure.Text != "")
+                    {
+                        dataStructuresArray[i, 0] = txtDataStrName.Text;
+                        dataStructuresArray[i, 2] = txtStructure.Text;
+                        StatusStripDataStr.Text = "Data Structure Name and Structure are edited!";
+                        break;
+                    }
+                    else if (selectedItem == ss && txtDataStrName.Text != "" && txtDefinition.Text != "")
+                    {
+                        dataStructuresArray[i, 0] = txtDataStrName.Text;
+                        dataStructuresArray[i, 3] = txtDefinition.Text;
+                        StatusStripDataStr.Text = "Data Structure Name and Definition are edited!";
+                        break;
+                    }
+                    else if (selectedItem == ss && txtStructure.Text != "" && txtDefinition.Text != "")
+                    {
+                        dataStructuresArray[i, 2] = txtStructure.Text;
+                        dataStructuresArray[i, 3] = txtDefinition.Text;
+                        StatusStripDataStr.Text = "Structure and Definition are edited!";
+                        break;
+                    }
+                    else if (selectedItem == ss && txtDataStrName.Text != "")
+                    {
+                        dataStructuresArray[i, 0] = txtDataStrName.Text;
+                        StatusStripDataStr.Text = "Data Structure Name is edited!";
+                        break;
+                    }
+                    else if (selectedItem == ss && txtCategory.Text != "")
+                    {
+                        dataStructuresArray[i, 1] = txtCategory.Text;
+                        StatusStripDataStr.Text = "Category is edited!";
+                        break;
+                    }
+                    else if (selectedItem == ss && txtStructure.Text != "")
+                    {
+                        dataStructuresArray[i, 2] = txtStructure.Text;
+                        StatusStripDataStr.Text = "Structure is edited!";
+                        break;
+                    }
+                    else if (selectedItem == ss && txtDefinition.Text != "")
+                    {
+                        dataStructuresArray[i, 3] = txtDefinition.Text;
+                        StatusStripDataStr.Text = "Definition is edited!";
+                        break;
+                    }
+                    else
+                    {
+                        StatusStripDataStr.Text = "No match found!";
+                    }
+
+                }
+            }
+            catch
+            {
+                StatusStripDataStr.Text = "Well that didn't work!";
+            }
+            
+        }
+
+        private void ButtonDelete_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ButtonLoad_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ButtonSave_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
